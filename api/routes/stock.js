24174667feee531753872aaa7c394966d3a2cd92
raystatic <rahul9650ray@gmail.com>
2021-01-20 23:15:25 +0530

    debugging

--- api/routes/stock.js
+++ api/routes/stock.js
@@ -13,11 +13,7 @@ const BSEAPI = api.BSE;
 router.get('/search',(req, res, next) => {
 
   const keyword = req.query.keyword;
-
-  // res.json({
-  //   data:keyword
-  // });
-
+  
   NSEAPI.searchStocks(keyword)
     .then((response) => {
       res.json({
