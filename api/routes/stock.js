5a78c5661d0ed51bad55662ab71fa4676f835d6f
raystatic <rahul9650ray@gmail.com>
2021-02-12 21:28:00 +0530

    order

--- api/routes/stock.js
+++ api/routes/stock.js
@@ -1,11 +1,12 @@
 const { json } = require('body-parser');
 const express = require('express');
 const router = express.Router();
-const api = require('indian-stock-exchange');
+const api = require('../../indian-stock-exchange');
 const connection = require('../../dbConfig');
 const {v4: uuidv4} = require('uuid');
 const OrderStatus = require('./orderStatus');
-const { update } = require('lodash');
+const { update, create, now } = require('lodash');
+var axios = require("axios").default;
 
 const NSEAPI = api.NSE;
 
@@ -51,7 +52,7 @@ router.get('/stock_info',(req, res, next) => {
 
 });
 
-const executeTransaction = (order) => {
+const executeTransaction = (order, userId) => {
   const updateOrderQuery = `UPDATE orders SET ? WHERE id= ?`;
   order.status = OrderStatus.executed;
   order.order_executed_at = new Date().getTime();
@@ -63,10 +64,450 @@ const executeTransaction = (order) => {
         console.log("order not find");
       }else{
         console.log("order updated");
+
+        const getUserDetails = "SELECT * FROM users WHERE id= ?"
+        connection.query(getUserDetails,[userId],(err, user) => {
+          if(err){
+            res.json({
+              error:true,
+              message:err.message
+            });
+          }else{
+            if(user === null || user.length === 0){
+              res.json({
+                error:true,
+                message:`Cannot find user`
+              });
+              return;
+            }
+
+          }
+        })
       }
     }
   });
 }
+
+const updateOrder = (order, res, user, type) => {
+  order.type = type
+  const updateOrderQuery = "UPDATE orders SET ?";
+  connection.query(updateOrderQuery,[order],(err, result) => {
+    if(err){
+      res.json({
+        error:true,
+        message:err
+      });
+    }else{
+      var balance = parseFloat(user[0].balance.replace(',',''));
+      var investment = parseFloat(user[0].investment.replace(',',''));
+      
+      if(order.type == "BUY"){
+        balance = balance - parseFloat(order.price);
+        investment = investment + parseFloat(order.price);
+      }else{
+        balance = balance + parseFloat(order.price);
+      }
+
+      console.log(`12feb update order ${balance} ${investment}`);
+
+      const updates = {balance:`${balance}`,investment:`${investment}`};
+      const updateBalanceQuery = `UPDATE users SET ? WHERE id= ?`;
+      connection.query(updateBalanceQuery,[updates, order.userId], (err, updatedUser) => {
+        if(err){
+          res.json({
+            error:true,
+            message:`Cannot update balance of user ${err}`
+          })
+        }else{
+          console.log("user's balance updated");
+          res.json({
+            error:false,
+            order:order
+          });
+        }
+      })
+      
+
+    }
+  })
+}
+
+const createOrder = (order, res, user) => {
+  const createOrderQuery = "INSERT INTO orders SET ?";
+  connection.query(createOrderQuery,[order],(err, result) => {
+    if(err){
+      res.json({
+        error:true,
+        message:err
+      });
+    }else{
+      var balance = parseFloat(user[0].balance.replace(',',''));
+      var investment = parseFloat(user[0].investment.replace(',',''));
+
+      console.log(`12feb create order before ${balance} ${investment}  ${parseFloat(order.price)}`);
+
+      if(order.type == "BUY"){
+        balance = parseFloat(balance) - parseFloat(order.price);
+        investment = parseFloat(investment) + parseFloat(order.price);
+      }else{
+        balance = balance + parseFloat(order.price);
+      }
+
+      console.log(`12feb create order ${balance} ${investment} `);
+
+      const updates = {balance:`${balance}`,investment:`${investment}`};
+      const updateBalanceQuery = `UPDATE users SET ? WHERE id= ?`;
+      connection.query(updateBalanceQuery,[updates, order.userId], (err, updatedUser) => {
+        if(err){
+          res.json({
+            error:true,
+            message:`Cannot update balance of user ${err}`
+          })
+        }else{
+          console.log("user's balance updated");
+          handleOrder(order)
+          res.json({
+            error:false,
+            order:order
+          });
+        }
+      })
+      
+
+    }
+  })
+}
+
+
+
+const handleOrder = (order) => {
+
+  if(order.product === "MIS"){
+    if(order.orderType === "MARKET"){
+      if(order.variety === "RGLR"){
+        if(order.validity === "DAY"){
+          //createOrder(order, res, user)
+          const now = new Date();
+          const eta_3PM = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 15, 0).getTime() - Date.now();
+          const eta_2min = new Date(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours(), now.getMinutes() + 2).getTime() - Date.now();
+
+          setTimeout(() => {
+            executeTransaction(order,order.userId)
+          }, eta_3PM)
+
+        }else if(order.validity === "IOC"){
+          executeTransaction(order,order.userId)
+        }
+      }else if(order.variety === "AMO"){
+        if(order.validity === "DAY"){
+          const now = new Date();
+          const eta_3PM = new Date(now.getFullYear(), now.getMonth(), now.getDate()+1, 15, 0).getTime() - Date.now();
+
+          setTimeout(() => {
+            executeTransaction(order,order.userId)
+          }, eta_3PM)
+
+        }else if(order.validity === "IOC"){
+          executeTransaction(order,order.userId)
+        }
+      }else if(order.validity === "BO"){
+
+        const now = new Date();
+
+        var interval = setInterval(() => {
+          NSEAPI.getQuoteInfo(order.symbol)
+                  .then((response) => {
+                      const currentPrice = parseFloat(response.data.data[0].lastPrice.replace(',',''))
+
+                      const stoploss = parseFloat(order.stopLoss);
+                      const target = parseFloat(order.target);
+
+                      const eachPrice = parseFloat(order.eachPrice);
+
+                      const eta_3PM = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 15, 0).getTime() - Date.now();
+
+                      if(eachPrice - currentPrice === stoploss * -1){
+                        if(order.type === "BUY"){
+                          updateOrder("SELL")
+                        }else{
+                          updateOrder("BUY")
+                        }
+                        clearInterval(interval)
+                      }else if(currentPrice === target){
+                        if(order.type === "BUY"){
+                          updateOrder("SELL")
+                        }else{
+                          updateOrder("BUY")
+                        }
+                        clearInterval(interval)
+                      }else if(eta_3PM <= 0){
+                        clearInterval(interval)
+                      }
+                  });
+        },3000)
+      }else if(order.variety === "CO"){
+        const now = new Date();
+        var interval = setInterval(() => {
+          NSEAPI.getQuoteInfo(order.symbol)
+                  .then((response) => {
+                      const currentPrice = parseFloat(response.data.data[0].lastPrice.replace(',',''))
+
+                      const stoplossTrigger = parseFloat(order.stoplossTrigger)
+
+                      const eta_3PM = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 15, 0).getTime() - Date.now();
+
+                      if(currentPrice + 1.5 === stoplossTrigger || currentPrice - 1.5 === stoplossTrigger){
+                        if(order.type === "BUY"){
+                          updateOrder("SELL")
+                        }else{
+                          updateOrder("BUY")
+                        }
+                        clearInterval(interval)
+                      }else if(eta_3PM <= 0){
+                        clearInterval(interval)
+                      }
+                  });
+        },3000)
+      }
+    }else if(order.product === "LIMIT"){
+
+      const now = new Date();
+
+      var interval = setInterval(() => {
+        NSEAPI.getQuoteInfo(order.symbol)
+                .then((response) => {
+                    const currentPrice = parseFloat(response.data.data[0].lastPrice.replace(',',''))
+
+                    const eachPrice = parseFloat(order.eachPrice)
+
+                    const eta_3PM = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 15, 0).getTime() - Date.now();
+
+                    if(currentPrice === eachPrice){
+                      if(order.type === "BUY"){
+                        updateOrder("SELL")
+                      }else{
+                        updateOrder("BUY")
+                      }
+                      clearInterval(interval)
+                    }else if(eta_3PM <= 0){
+                      clearInterval(interval)
+                    }
+                });
+      },3000)
+    }else if(order.product === "SL" || order.product === "SLM"){
+      const now = new Date();
+      var interval = setInterval(() => {
+        NSEAPI.getQuoteInfo(order.symbol)
+                .then((response) => {
+                    const currentPrice = parseFloat(response.data.data[0].lastPrice.replace(',',''))
+
+                    const stopLoss = parseFloat(order.stopLoss)
+
+                    const eachPrice = parseFloat(order.eachPrice)
+
+                    const eta_3PM = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 15, 0).getTime() - Date.now();
+
+                    if(currentPrice - eachPrice === stopLoss){
+                      if(order.type === "BUY"){
+                        updateOrder("SELL")
+                      }else{
+                        updateOrder("BUY")
+                      }
+                      clearInterval(interval)
+                    }else if(eta_3PM <= 0){
+                      clearInterval(interval)
+                    }
+                });
+      },3000)
+    }
+}else if(order.product === "CNC"){
+  if(order.orderType === "MARKET"){
+    if(order.variety === "RGLR"){
+      if(order.validity === "DAY"){
+        createOrder(order, res, user)
+        const now = new Date();
+        const eta_3PM = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 15, 0).getTime() - Date.now();
+        const eta_2min = new Date(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours(), now.getMinutes() + 2).getTime() - Date.now();
+
+        setTimeout(() => {
+          executeTransaction(order,order.userId)
+        }, eta_3PM)
+
+      }else if(order.validity === "IOC"){
+        executeTransaction(order,order.userId)
+      }
+    }else if(order.variety === "AMO"){
+      if(order.validity === "DAY"){
+        const now = new Date();
+        const eta_3PM = new Date(now.getFullYear(), now.getMonth(), now.getDate()+1, 15, 0).getTime() - Date.now();
+
+        setTimeout(() => {
+          executeTransaction(order,order.userId)
+        }, eta_3PM)
+
+      }else if(order.validity === "IOC"){
+        executeTransaction(order,order.userId)
+      }
+    }else if(order.validity === "BO"){
+
+      const now = new Date();
+
+      var interval = setInterval(() => {
+        NSEAPI.getQuoteInfo(order.symbol)
+                .then((response) => {
+                    const currentPrice = parseFloat(response.data.data[0].lastPrice.replace(',',''))
+
+                    const stoploss = parseFloat(order.stopLoss);
+                    const target = parseFloat(order.target);
+
+                    const eachPrice = parseFloat(order.eachPrice);
+
+                    const eta_3PM = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 15, 0).getTime() - Date.now();
+
+                    if(eachPrice - currentPrice === stoploss * -1){
+                      if(order.type === "BUY"){
+                        updateOrder("SELL")
+                      }else{
+                        updateOrder("BUY")
+                      }
+                      clearInterval(interval)
+                    }else if(currentPrice === target){
+                      if(order.type === "BUY"){
+                        updateOrder("SELL")
+                      }else{
+                        updateOrder("BUY")
+                      }
+                      clearInterval(interval)
+                    }else if(eta_3PM <= 0){
+                      clearInterval(interval)
+                    }
+                });
+      },3000)
+    }else if(order.variety === "CO"){
+      const now = new Date();
+      var interval = setInterval(() => {
+        NSEAPI.getQuoteInfo(order.symbol)
+                .then((response) => {
+                    const currentPrice = parseFloat(response.data.data[0].lastPrice.replace(',',''))
+
+                    const stoplossTrigger = parseFloat(order.stoplossTrigger)
+
+                    const eta_3PM = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 15, 0).getTime() - Date.now();
+
+                    if(currentPrice + 1.5 === stoplossTrigger || currentPrice - 1.5 === stoplossTrigger){
+                      if(order.type === "BUY"){
+                        updateOrder("SELL")
+                      }else{
+                        updateOrder("BUY")
+                      }
+                      clearInterval(interval)
+                    }else if(eta_3PM <= 0){
+                      clearInterval(interval)
+                    }
+                });
+      },3000)
+    }
+  }else if(order.product === "LIMIT"){
+
+    const now = new Date();
+
+    var interval = setInterval(() => {
+      NSEAPI.getQuoteInfo(order.symbol)
+              .then((response) => {
+                  const currentPrice = parseFloat(response.data.data[0].lastPrice.replace(',',''))
+
+                  const eachPrice = parseFloat(order.eachPrice)
+
+                  const eta_3PM = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 15, 0).getTime() - Date.now();
+
+                  if(currentPrice === eachPrice){
+                    if(order.type === "BUY"){
+                      updateOrder("SELL")
+                    }else{
+                      updateOrder("BUY")
+                    }
+                    clearInterval(interval)
+                  }else if(eta_3PM <= 0){
+                    clearInterval(interval)
+                  }
+              });
+    },3000)
+  }else if(order.product === "SL" || order.product === "SLM"){
+    const now = new Date();
+    var interval = setInterval(() => {
+      NSEAPI.getQuoteInfo(order.symbol)
+              .then((response) => {
+                  const currentPrice = parseFloat(response.data.data[0].lastPrice.replace(',',''))
+
+                  const stopLoss = parseFloat(order.stopLoss)
+
+                  const eachPrice = parseFloat(order.eachPrice)
+
+                  const eta_3PM = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 15, 0).getTime() - Date.now();
+
+                  if(currentPrice - eachPrice === stopLoss){
+                    if(order.type === "BUY"){
+                      updateOrder("SELL")
+                    }else{
+                      updateOrder("BUY")
+                    }
+                    clearInterval(interval)
+                  }else if(eta_3PM <= 0){
+                    clearInterval(interval)
+                  }
+              });
+    },3000)
+  }
+}
+
+
+}
+
+
+router.post('/order',(req, res, next) => {
+  const order = {
+    id:uuidv4(),
+    symbol:req.body.symbol,
+    product:req.body.product,
+    type:req.body.type,    // buy/sell
+    variety:req.body.variety,
+    noOfShares:req.body.noOfShares,
+    userId:req.body.userId,
+    orderType:req.body.orderType, // market/limit/SL/SLM
+    price:req.body.price,
+    stopLoss:req.body.stopLoss,
+    triggeredPrice:req.body.triggeredPrice,
+    target:req.body.target,
+    validity:req.body.validity,
+    stoplossTrigger:req.body.stoplossTrigger,
+    trailingStoploss:req.body.trailingStoploss,
+    status:"PENDING",
+    eachPrice:req.body.eachPrice,
+    order_created_at:new Date().getTime()
+  };
+  
+  const getUserDetails = "SELECT * FROM users WHERE id= ?"
+  connection.query(getUserDetails,[order.userId],(err, user) => {
+    if(err){
+      res.json({
+        error:true,
+        message:err.message
+      });
+    }else{
+      if(user === null || user.length === 0){
+        res.json({
+          error:true,
+          message:`Cannot find user`
+        });
+        return;
+      }
+
+      createOrder(order, res, user)
+
+    }
+  })
+
+});
 
 router.post('/transaction', (req, res, next) => {
     const order = {
@@ -437,7 +878,7 @@ async function fetchCurrentPrice(orders, res){
 router.get('/transaction', (req, res, next) => {
   const userId = req.query.userId
 
-  const getOrderQuery = `SELECT * FROM orders WHERE user_id= ?`;
+  const getOrderQuery = `SELECT * FROM orders WHERE userId= ?`;
   connection.query(getOrderQuery,[userId],(err, result) => {
     if(err){
       res.json({
@@ -459,18 +900,56 @@ router.get('/transaction', (req, res, next) => {
 
 });
 
+
+
 router.get('/futures',(req, res, next) => {
-  NSEAPI.getIndexFuturesData("RELIANCE")
+  const symbol = req.query.symbol;
+  NSEAPI.getFuturesData(symbol)
     .then((response) => {
+      const d = []
+      for(i in response){
+        d.push([response[i]]);
+      }
+
       res.json({
-        futures:response
+        error:false,
+        futures:d
       });
     })
     .catch((err)=> {
       res.json({
-        error:err
+        error:true,
+        message:err
       });
     })
 });
+
+router.get('/options',(req, res, next) => {
+  const symbol = req.query.symbol;
+  NSEAPI.getOptionsData(symbol)
+    .then((response) => {
+
+      const d = []
+      for(i in response){
+        d.push({
+          date:i,
+          data:response[i]
+        });
+      }
+
+      res.json({
+        error:false,
+        options:d
+      });
+    })
+    .catch((err)=> {
+      res.json({
+        error:true,
+        message:err
+      });
+    })
+});
+
+
 
 module.exports = router;
