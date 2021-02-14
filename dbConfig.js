3f0056b7fe74160f1a2f43a3b08864efc8e387ae
raystatic <rahul9650ray@gmail.com>
2021-02-15 00:23:36 +0530

    cred

--- dbConfig.js
+++ dbConfig.js
@@ -6,15 +6,17 @@ dotenv.config();
 const connection = mysql.createConnection({
     host:process.env.DB_HOST,
     user:process.env.DB_USERNAME,
-    password:"",
+    password:process.env.DB_PASSWORD,
     database:process.env.DB_NAME
 });
 
 connection.connect((err) => {
     if(err){
-        console.log(`db not connected ${err.message}`);
+        console.log(`db not connected ${err}`);
+    }else{
+        console.log("db connected!");
     }
-    console.log("db connected!");
+    //console.log("db connected!");
 })
 
 module.exports = connection;
